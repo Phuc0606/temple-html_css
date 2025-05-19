41ce78ae94bfd6f128b84ffeee1b4415cfc39ab4
Phuc0606 <thanhphuchuynh658@gmail.com>
2025-05-19 17:23:13 +0700

    first

---
+++ 2280602431_Phuc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _2280602431_Phuc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
